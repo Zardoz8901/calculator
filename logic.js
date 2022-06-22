1dd6f6acf531d181491912b864496229c85b1931
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-06-22 13:57:28 +0200

    Add comments to establish workflow narrative

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 // create calculator template
 class Calculator {
     
-    // establish operand text parameters and call function clear
+    // assign operand text parameters and call function clear
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
@@ -22,28 +22,41 @@ class Calculator {
 
     }
 
-    // calls the number button inner text as function argument
+    // call the number button inner text as function argument
     appendNumber(number) {
+        // number converted to string
         this.currentOperand = this.currentOperand.toString()
+        // limit input to a single decimal
         if (number === '.' && this.currentOperand.includes('.')) return
+        // add the inputted number to current operand
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
+    // call the operation button inner text as function arguement
     chooseOperation(operation) {
+        // if current operand is empty, stop
         if (this.currentOperand === '') return
+        // if previous operand is not empty call compute function
         if (this.previousOperand !== '') {
             this.compute()
         }
         this.operation = operation
+        // set previous operand to current operand value
         this.previousOperand = this.currentOperand
+        // clear current operand value
         this.currentOperand = ''
     }
 
+    // pass operands and operations through compute
     compute() {
+        // assign computation variable to scope
         let computation
+        // convert operand strings to floating point numbers 
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
+        // if neither operand is a number, stop
         if (isNaN(prev) || isNaN(current)) return
+        // compute prev and current operands add, subtract, divide, multiply
         switch (this.operation) {
             case '+':
                 computation = prev + current
@@ -60,35 +73,58 @@ class Calculator {
             default: 
                 return        
         }
+        // set computed operands as current operand
         this.currentOperand = computation
+        // clear the active operator
         this.operation = undefined
+        // set previous operand to empty
         this.previousOperand = ''
     }
 
+    // helper function to comb the kinks of decimals and display aesthetic
+    // current operand is passed through as an argument
+    // integer digits and decimal digits are seperated and processed individually
+    // integer digits and decimal digits are then catenated into a grammaticly correct whole 
     getDisplayNumber(number) {
+        // convert current operand to string
         const stringNumber = number.toString()
+        // break off all numbers preceding the decimal
         const integerDigits = parseFloat(stringNumber.split('.')[0])
+        // break off all numbers following the decimal
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay
+        // if integer digits are not a number return empty string
         if (isNaN(integerDigits)) {
             integerDisplay = ''
         } else {
+            // if integer digits are a number format visual aesthetic to english grammar convention
+            // set maximum fraction digits to 0 to remove decimal places from integer digits
             integerDisplay = integerDigits.toLocaleString('en', {
                 maximumFractionDigits: 0 })
         }
+        // if decimal digit is a number add it to the integer display
         if (decimalDigits != null) {
             return `${integerDisplay}.${decimalDigits}`
+        // if not return integer display number
         } else {
             return integerDisplay
         }
     }
 
+    // display the integers and operators
     updateDisplay() {
+        // current operand inner text is set to the current operand variable
+        // after variable is passed through the display number cleaner function
         this.currentOperandTextElement.innerText = 
         this.getDisplayNumber(this.currentOperand)
+        // if there is an operator called
         if (this.operation != null) {
+            // set previous operand inner text is set to previous operand variabled
+            // the variable is passed throught the display number cleaner function
+            // the operation symbol is catenated at the end
             this.previousOperandTextElement.innerText =
             `${this.getDisplayNumber(this.previousOperand)} ${this.operation }`
+        // if operation is null the previous operand text is cleared
         } else {
             this.previousOperandTextElement.innerText = ''
         }
